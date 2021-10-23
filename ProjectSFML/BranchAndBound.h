690bfefaf76f85a7515734c0ab0d96f9287edd3c
astatochek <71458498+astatochek@users.noreply.github.com>
2021-10-23 12:59:18 +0300

    Delete ProjectSFML directory

--- ProjectSFML/BranchAndBound.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-
-#include "Graph.h"
-
-std::pair <std::vector <int>, int> BranchAndBound(Graph* graph);
--- ProjectSFML/antColony.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include <utility>
-
-std::pair <std::vector<int>, int> ant_colony(std::vector <std::vector<int>> Mprev);
